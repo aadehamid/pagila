495f5bbd633c2f4a741681ca8ea7516ef08ae794
Hamid Adesokan <aadehamid@gmail.com>
2024-05-27 23:32:07 -0500

    Re exported custemrs data to .csv

--- northwind/export_csv.sql
+++ northwind/export_csv.sql
@@ -8,7 +8,8 @@ COPY (SELECT * FROM categories) TO '/var/lib/postgresql/data/northwind_csv/categ
 COPY (SELECT * FROM shippers) TO '/var/lib/postgresql/data/northwind_csv/shippers.csv' WITH CSV header;
 
 COPY (SELECT * FROM orders LEFT OUTER JOIN order_details ON order_details.OrderID = orders.OrderID) TO '/var/lib/postgresql/data/northwind_csv/orders.csv' WITH CSV header;
-COPY (SELECT * FROM customers LEFT OUTER JOIN customercustomerdemo ON customercustomerdemo.customerID = customers.customerID LEFT OUTER JOIN  customerdemographics on customerdemographics.customertypeid = customercustomerdemo.customertypeid) TO '/var/lib/postgresql/data/northwind_csv/customers.csv' WITH CSV header;
+-- COPY (SELECT * FROM customers LEFT OUTER JOIN customercustomerdemo ON customercustomerdemo.customerID = customers.customerID LEFT OUTER JOIN  customerdemographics on customerdemographics.customertypeid = customercustomerdemo.customertypeid) TO '/var/lib/postgresql/data/northwind_csv/customers.csv' WITH CSV header;
 
 COPY (SELECT * FROM employeeterritories LEFT OUTER JOIN territories ON territories.territoryid = employeeterritories.territoryid) TO '/var/lib/postgresql/data/northwind_csv/employeeterritories.csv' WITH CSV header;
 COPY (SELECT * FROM region) TO '/var/lib/postgresql/data/northwind_csv/region.csv' WITH CSV header;
+COPY (SELECT * FROM customers) TO '/var/lib/postgresql/data/northwind_csv/customers.csv' WITH CSV header;
